905095664d4e3b473be2e555720a4c693f3c6e1a
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2024-04-02 22:33:33 -0300

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
-import { colors } from "./src/styles/colors";
-import { fontFamily } from "./src/styles/fontFamily";
+const colors = {
+  white: "#E1E1E6",
+
+  gray: {
+    200: "#C4C4CC",
+    300: "#8D8D99",
+  },
+
+  green: {
+    500: "#00292E",
+    400: "#28494E",
+    200: "#9FF9CC",
+  },
+  orange: {
+    500: "#F48F56",
+  },
+};
+
+const fontFamily = {
+  regular: "Roboto_400Regular",
+  bold: "Roboto_700Bold",
+  medium: "Roboto_500Medium",
+};
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
-  presets: [require("nativewind/preset")],
   theme: {
     extend: { colors, fontFamily },
   },
